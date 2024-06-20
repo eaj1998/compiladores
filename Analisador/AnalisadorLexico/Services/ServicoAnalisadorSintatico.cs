88a66ea5b4977b0526aab4c66a78db862bc9eb07
eaj1998 <edipo1998@gmail.com>
2024-06-20 14:39:21 -0300

    Comentarios para ajudar na apresentacao

--- Analisador/AnalisadorLexico/Services/ServicoAnalisadorSintatico.cs
+++ Analisador/AnalisadorLexico/Services/ServicoAnalisadorSintatico.cs
@@ -20,14 +20,14 @@ namespace Servicos {
         }
 
         public void lerSimbolos() {
-            // le os simbolos que o gold parser enumera
-            System.Xml.XmlNodeList xmlNodeList = xmlTabela.GetElementsByTagName("Symbol");
-            tabelaLALR.simbolos = new Simbolo[xmlNodeList.Count + 1];
-            for (int i = 0; i < xmlNodeList.Count; i++) {
-                Simbolo simbolo = new Simbolo();
-                simbolo.indice = int.Parse(xmlNodeList[i].Attributes["Index"].Value);
-                simbolo.nome = xmlNodeList[i].Attributes["Name"].Value;
-                tabelaLALR.simbolos[i] = simbolo;
+            System.Xml.XmlNodeList xmlNodeList = xmlTabela.GetElementsByTagName("Symbol"); // Obtém uma lista de nós XML que possuem a tag "Symbol".
+            tabelaLALR.simbolos = new Simbolo[xmlNodeList.Count + 1]; // Inicializa o array de símbolos com o tamanho da lista de nós + 1.
+            for (int i = 0; i < xmlNodeList.Count; i++)
+            { // Itera sobre cada nó na lista.
+                Simbolo simbolo = new Simbolo(); // Cria um novo objeto Simbolo.
+                simbolo.indice = int.Parse(xmlNodeList[i].Attributes["Index"].Value); // Define o índice do símbolo a partir do atributo XML.
+                simbolo.nome = xmlNodeList[i].Attributes["Name"].Value; // Define o nome do símbolo a partir do atributo XML.
+                tabelaLALR.simbolos[i] = simbolo; // Armazena o símbolo no array.
             }
         }
 
@@ -35,108 +35,126 @@ namespace Servicos {
             // le as produções que o gold parser enumera
             // guarda tambem os símbolos de cada produção
             // usa na redução para saber o tamanho da produção e o simbolo que da nome a regra          
-            producoes = new List<Producao>();
-            System.Xml.XmlNodeList xmlNodeList = xmlTabela.GetElementsByTagName("Production");
-            foreach (System.Xml.XmlNode n in xmlNodeList) {
-                Producao producao = new Producao();
-                producao.indice = Int32.Parse(n.Attributes["Index"].InnerText);
-                producao.indiceNaoTerminal = int.Parse(n.Attributes["NonTerminalIndex"].InnerText);
-                producao.simbolosProducao = new List<int>();
-                foreach (System.Xml.XmlNode s in n.ChildNodes) {
-                    producao.simbolosProducao.Add(int.Parse(s.Attributes["SymbolIndex"].InnerText));
+            producoes = new List<Producao>(); // Inicializa a lista de produções.
+            System.Xml.XmlNodeList xmlNodeList = xmlTabela.GetElementsByTagName("Production"); // Obtém uma lista de nós XML que possuem a tag "Production".
+            foreach (System.Xml.XmlNode n in xmlNodeList)
+            { // Itera sobre cada nó na lista.
+                Producao producao = new Producao(); // Cria um novo objeto Producao.
+                producao.indice = Int32.Parse(n.Attributes["Index"].InnerText); // Define o índice da produção a partir do atributo XML.
+                producao.indiceNaoTerminal = int.Parse(n.Attributes["NonTerminalIndex"].InnerText); // Define o índice do não-terminal a partir do atributo XML.
+                producao.simbolosProducao = new List<int>(); // Inicializa a lista de símbolos da produção.
+                foreach (System.Xml.XmlNode s in n.ChildNodes)
+                { // Itera sobre os nós filhos da produção.
+                    producao.simbolosProducao.Add(int.Parse(s.Attributes["SymbolIndex"].InnerText)); // Adiciona o índice do símbolo na lista de símbolos da produção.
                 }
-                producoes.Add(producao);
+                producoes.Add(producao); // Adiciona a produção na lista de produções.
             }
-
         }
 
         public void lerTabelaLALR() {
-            // le a tabela que o gold parser gera
-            System.Xml.XmlNodeList xmlNodeList = xmlTabela.GetElementsByTagName("LALRState");
+            System.Xml.XmlNodeList xmlNodeList = xmlTabela.GetElementsByTagName("LALRState"); // Obtém uma lista de nós XML que possuem a tag "LALRState".
 
-            tabelaLALR.estados = new int[xmlNodeList.Count];
-            tabelaLALR.acoes = new AcaoTabelaLALR[tabelaLALR.estados.Count(), tabelaLALR.simbolos.Count() + 1];
+            tabelaLALR.estados = new int[xmlNodeList.Count]; // Inicializa o array de estados com o tamanho da lista de nós.
+            tabelaLALR.acoes = new AcaoTabelaLALR[tabelaLALR.estados.Count(), tabelaLALR.simbolos.Count() + 1]; // Inicializa a matriz de ações com base na quantidade de estados e símbolos.
 
-            foreach (System.Xml.XmlNode nodoEstado in xmlNodeList) {
-                int indiceEstado = int.Parse(nodoEstado.Attributes["Index"].InnerText);
-                tabelaLALR.estados[indiceEstado] = indiceEstado;
+            foreach (System.Xml.XmlNode nodoEstado in xmlNodeList)
+            { // Itera sobre cada nó de estado na lista.
+                int indiceEstado = int.Parse(nodoEstado.Attributes["Index"].InnerText); // Obtém o índice do estado a partir do atributo XML.
+                tabelaLALR.estados[indiceEstado] = indiceEstado; // Define o estado no array de estados.
 
-                foreach (System.Xml.XmlNode nodoAcao in nodoEstado.ChildNodes) {
-                    int indiceAcao = int.Parse(nodoAcao.Attributes["SymbolIndex"].InnerText);
-                    AcaoTabelaLALR acao = new AcaoTabelaLALR();
-                    Enum.TryParse(nodoAcao.Attributes["Action"].InnerText, out acao.acao);
-                    acao.valor = int.Parse(nodoAcao.Attributes["Value"].InnerText);
-                    tabelaLALR.acoes[indiceEstado, indiceAcao] = acao;
+                foreach (System.Xml.XmlNode nodoAcao in nodoEstado.ChildNodes)
+                { // Itera sobre os nós filhos do estado.
+                    int indiceAcao = int.Parse(nodoAcao.Attributes["SymbolIndex"].InnerText); // Obtém o índice da ação a partir do atributo XML.
+                    AcaoTabelaLALR acao = new AcaoTabelaLALR(); // Cria um novo objeto AcaoTabelaLALR.
+                    Enum.TryParse(nodoAcao.Attributes["Action"].InnerText, out acao.acao); // Converte o valor do atributo "Action" para o tipo enumerado.
+                    acao.valor = int.Parse(nodoAcao.Attributes["Value"].InnerText); // Define o valor da ação a partir do atributo XML.
+                    tabelaLALR.acoes[indiceEstado, indiceAcao] = acao; // Armazena a ação na matriz de ações.
                 }
-
             }
         }
 
-        public List<string> analisar(List<TabelaSimbolos> tokens) {
-            List<string> listaRetorno = new List<string>();
+        public List<string> analisar(List<TabelaSimbolos> tokens)
+        {
+            List<string> listaRetorno = new List<string>(); // Inicializa a lista de strings para armazenar mensagens de erro ou sucesso.
 
-            List<int> fita = mapearFitaSaidaAnaliseSintatica(tokens);
-            List<int> pilha = new List<int>();
-            int indiceLeituraFita = 0;
+            List<int> fita = mapearFitaSaidaAnaliseSintatica(tokens); // Mapeia os tokens para uma lista de índices de símbolos.
+            List<int> pilha = new List<int>(); // Inicializa a pilha de estados.
+            int indiceLeituraFita = 0; // Inicializa o índice de leitura da fita.
 
-            pilha.Add(tabelaLALR.estados[0]);
+            pilha.Add(tabelaLALR.estados[0]); // Adiciona o estado inicial à pilha.
 
-            while (true) {
-                int estadoTopoPilha = pilha.Last();
-                int tokenAtualFita = fita[indiceLeituraFita];
+            while (true)
+            { // Loop principal de análise.
+                int estadoTopoPilha = pilha.Last(); // Obtém o estado no topo da pilha.
+                int tokenAtualFita = fita[indiceLeituraFita]; // Obtém o token atual da fita.
 
-                AcaoTabelaLALR acaoTabela = tabelaLALR.acoes[estadoTopoPilha, tokenAtualFita];
+                AcaoTabelaLALR acaoTabela = tabelaLALR.acoes[estadoTopoPilha, tokenAtualFita]; // Obtém a ação correspondente na tabela LALR.
 
-                if (acaoTabela == null) {
-                    if (indiceLeituraFita == tokens.Count) {
+                if (acaoTabela == null)
+                { // Se a ação não for encontrada, ocorre um erro.
+                    if (indiceLeituraFita == tokens.Count)
+                    { // Se todos os tokens foram lidos, é um erro de fim de arquivo inesperado.
                         TabelaSimbolos token = tokens[indiceLeituraFita - 1];
-                        listaRetorno.Add("Fim de arquivo inesperado. O token " + token.rotulo + ", na linha " + token.linha + " não um conclui uma expressão.");
-                    } else {
+                        listaRetorno.Add("Fim de arquivo inesperado. O token " + token.rotulo + ", na linha " + token.linha + " não conclui uma expressão.");
+                    }
+                    else
+                    { // Caso contrário, é um token inesperado.
                         TabelaSimbolos token = tokens[indiceLeituraFita];
                         listaRetorno.Add("Token " + token.rotulo + " inesperado na linha " + token.linha + ".");
                     }
                     break;
-                } else if (acaoTabela.acao == Enumeradores.Acao.Aceitar) {
+                }
+                else if (acaoTabela.acao == Enumeradores.Acao.Aceitar)
+                { // Se a ação for "Aceitar", a análise está concluída com sucesso.
                     break;
-                } else if (acaoTabela.acao == Enumeradores.Acao.Shift) {
+                }
+                else if (acaoTabela.acao == Enumeradores.Acao.Shift)
+                { // Se a ação for "Shift", move o token atual para a pilha e avança para o próximo token.
                     pilha.Add(acaoTabela.valor);
                     indiceLeituraFita += 1;
-                } else if (acaoTabela.acao == Enumeradores.Acao.Reduce) {
+                }
+                else if (acaoTabela.acao == Enumeradores.Acao.Reduce)
+                { // Se a ação for "Reduce", aplica a produção de redução correspondente.
                     Producao producaoReducao = producoes[acaoTabela.valor];
-                    if (producaoReducao != null) {
-                        int tamanhoProducao = producaoReducao.simbolosProducao.Count();
-                        pilha.RemoveRange(pilha.Count() - tamanhoProducao, tamanhoProducao);
-                        pilha.Add(tabelaLALR.acoes[pilha.Last(), producaoReducao.indiceNaoTerminal].valor);
-                    } else {
-                        listaRetorno.Add("Produção de redução não encontrada.");
+                    if (producaoReducao != null)
+                    {
+                        int tamanhoProducao = producaoReducao.simbolosProducao.Count(); // Obtém o tamanho da produção.
+                        pilha.RemoveRange(pilha.Count() - tamanhoProducao, tamanhoProducao); // Remove os estados da pilha correspondentes ao tamanho da produção.
+                        pilha.Add(tabelaLALR.acoes[pilha.Last(), producaoReducao.indiceNaoTerminal].valor); // Adiciona o estado resultante da redução.
+                    }
+                    else
+                    {
+                        listaRetorno.Add("Produção de redução não encontrada."); // Se a produção de redução não for encontrada, ocorre um erro.
                         break;
                     }
                 }
 
-                if (indiceLeituraFita == fita.Count()) {
+                if (indiceLeituraFita == fita.Count())
+                { // Se a fita de análise terminou e não foi reconhecida, ocorre um erro.
                     listaRetorno.Add("Fim da fita de análise... O código não foi reconhecido.");
                     break;
                 }
-
             }
 
-            return listaRetorno;
+            return listaRetorno; // Retorna a lista de mensagens de erro ou sucesso.
         }
 
-        private List<int> mapearFitaSaidaAnaliseSintatica(List<TabelaSimbolos> tokens) {
-            // a partir da fita de saida do analisador lexico
-            // gera uma nova fita mapeada com os indices gerados pelo gold parser
+
+        private List<int> mapearFitaSaidaAnaliseSintatica(List<TabelaSimbolos> tokens)
+        {
             Simbolo simbolo;
             List<int> fita = new List<int>();
-            foreach (TabelaSimbolos token in tokens) {
-                simbolo = tabelaLALR.getSimboloPorIndice(token.getIndiceSimboloGoldParser);
-                if (simbolo != null) {
-                    fita.Add(simbolo.indice);
+            foreach (TabelaSimbolos token in tokens)
+            { // Itera sobre cada token na lista.
+                simbolo = tabelaLALR.getSimboloPorIndice(token.getIndiceSimboloGoldParser); // Obtém o símbolo correspondente ao índice do token.
+                if (simbolo != null)
+                {
+                    fita.Add(simbolo.indice); // Adiciona o índice do símbolo à fita.
                 }
             }
-            simbolo = tabelaLALR.getSimboloPorIndice(0); // EOF
-            fita.Add(simbolo.indice);
-            return fita;
+            simbolo = tabelaLALR.getSimboloPorIndice(0); // Obtém o símbolo EOF.
+            fita.Add(simbolo.indice); // Adiciona o índice do símbolo EOF à fita.
+            return fita; // Retorna a fita mapeada.
         }
 
     }
